d72acd37c022f4971ff441c377fb978ee07058df
Peter K Konneker <konnekerp@law.byu.edu>
2011-10-27 21:36:47 -0600

    weird spacing fix

--- render.cpp
+++ render.cpp
@@ -48,9 +48,9 @@ void game_render::gameRender::bindTexture(string & filename, game_model & model)
     BMPLoad(filename, model.getTexHolder());
     glBindTexture(GL_TEXTURE_2D, model.getTexture());
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-    glTexParameteri (GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-    glTexParameteri (GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-    glTexParameteri (GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
     glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
     glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, model.getTexHolder().width,
         model.getTexHolder().height, 0, GL_RGB, GL_UNSIGNED_BYTE,
